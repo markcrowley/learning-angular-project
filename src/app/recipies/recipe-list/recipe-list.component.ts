4fc83ca9b2a9f6116b885a321f6718a7b1a781fb
Mark Crowley <markcrowley@gmail.com>
2017-05-05 23:28:20 +0100

    Added Jacobs recipie

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -13,6 +13,8 @@ export class RecipeListComponent implements OnInit {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Traditional.Sunday.Roast-01-cropped.jpg/300px-Traditional.Sunday.Roast-01-cropped.jpg'),
     new Recipe('Vegetarian option', 'Vegetarian',
       'https://upload.wikimedia.org/wikipedia/commons/3/3e/Mango_Vegetarian_Stir_Fry.jpg'),
+    new Recipe('Jacob special', 'Steak and mash',
+      'http://wdy.h-cdn.co/assets/cm/15/09/54ef889c0ee63_-_blackened-skirt-steak-with-cheddar-sage-mashed-potatoes-lg.jpg'),
   ];
 
   constructor() { }
